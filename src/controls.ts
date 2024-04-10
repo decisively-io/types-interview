cf512c178f6f2793fa8b9d1307d1e28daf3bfa07
dylan <dylan.sperotto@imminently.co>
2024-04-10 16:01:39 +0200

    UPD: add `columnHeading` and `columnWidth` props to `TypographyControl`

--- src/controls.ts
+++ src/controls.ts
@@ -368,6 +368,10 @@ export interface TypographyControl extends BaseControl {
   text: string;
   style: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "subtitle1" | "subtitle2" | "body1" | "body2" | "caption" | "banner-green" | "banner-yellow" | "banner-red";
   emoji?: string;
+  /** `columnHeading` is relevant only when nested within a repeat_container, and will otherwise be ignored */
+  columnHeading?: string;
+  /** `columnWidth` is relevant only when nested within a repeat_container, and will otherwise be ignored */
+  columnWidth?: number;
 }
 
 /**
